f63dcc31d49e4ddf8b526b495b98ff7f306aa67c
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-11-03 19:14:25 +0600

    This is the next

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -9,18 +9,59 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 
-//HdEBa2fslrhZXNB4
-//geniusdb-user
 
+//GeniusCar  Y37oo9QJu06vp0sB
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ksaovkw.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+const uri = "mongodb+srv://GeniusCar:Y37oo9QJu06vp0sB@cluster0.ksaovkw.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const servicesCollection = client.db("GeniusCarDB").collection("services");
+        const ordersCollection = client.db("GeniusCarDB").collection("orders");
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const services = await servicesCollection.find(query).toArray();
+            res.send(services);
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
+            const orders = await ordersCollection.find(query).toArray();
+            res.send(orders);
+
+        })
+
+        //orders api
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result);
+        })
+
+
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(error => console.log(error));
+
 
 
 
